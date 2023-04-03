5e96d9f50cc0d5adf925413529f8d2ff4b62d681
Alan Funes <funesalan6@gmail.com>
2023-04-03 17:42:46 -0600

    Se sube todo sobre Materia prima

--- app/models/feedstock.py
+++ app/models/feedstock.py
@@ -9,17 +9,15 @@ class Feedstock:
     price = 0.0,
     min_value = 0.0,
     max_value = 0.0,
-    status = 0,
     measurement_unit_id = Measurement_Unit.id
     provider_id = Provider.id
 
-    def __init__(self, id, name, description, price, status, min_value, max_value, measurement_unit_id, provider_id):
+    def __init__(self, id, name, description, price, min_value, max_value, measurement_unit_id, provider_id):
         self.id = id
         self.name = name
         self.description = description
         self.price = price
         self.min_value = min_value
         self.max_value = max_value
-        self.status = status
         self.measurement_unit_id = measurement_unit_id
         self.provider_id = provider_id
